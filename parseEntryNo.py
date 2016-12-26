6c51dbafdb909e802c270811d0696f715e7389c8
Aman Agrawal <aman71197@gmail.com>
2016-12-26 23:56:49 +0530

    parsing entry number

---
+++ parseEntryNo.py
@@ -0,0 +1,14 @@
+
+def parseEntryNo(entryno):
+	year = int(entryno[:4])
+	dept = entryno[4:7]
+	return year, dept
+
+def main():
+	entry = raw_input("Enter entry number\n")
+	year, dept = parseEntryNo(entry)
+	print "Entry Year is", year
+	print "Department is", dept
+
+if __name__ == '__main__':
+	main()
