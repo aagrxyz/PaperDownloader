9c704821491dea9e14c806aed557b3da54a90105
ozym4nd145 <ozym4nd145@outlook.com>
2017-01-08 22:53:16 +0530

    Add Old Courses Database

--- reorganise_database.py
+++ reorganise_database.py
@@ -1,20 +1,20 @@
 import os
 
-baaproot = "C:\Users\SAKET\Desktop\QuestionPapers"
+baaproot = "./QuestionPapers"
 for file in os.listdir(baaproot):
-	root  =  baaproot + "\\" + file
+	root  =  baaproot + "/" + file
 	some_var = "aaaaaa"
 	for file in os.listdir(root):
 		if file.endswith(".pdf"):
 			some_var1 = file[:6].upper()
 			if some_var == some_var1:
-				oldpath = root + "\\" + file
-				newpath = root + "\\" + some_var1 + "\\" + file
+				oldpath = root + "/" + file
+				newpath = root + "/" + some_var1 + "/" + file
 				os.rename(oldpath,newpath) 
 			else:
-				os.makedirs(root + "\\" + some_var1)
-				oldpath = root + "\\" + file
-				newpath = root + "\\" + some_var1 + "\\" + file
+				os.makedirs(root + "/" + some_var1)
+				oldpath = root + "/" + file
+				newpath = root + "/" + some_var1 + "/" + file
 				os.rename(oldpath,newpath) 
 				some_var = some_var1
 
