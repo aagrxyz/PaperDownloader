fa9752f9074478331da826eaad760a32e60e9a75
Aman Agrawal <aman71197@gmail.com>
2017-01-08 22:55:36 +0530

    update re_organise

--- reorganise_database.py
+++ reorganise_database.py
@@ -1,20 +1,23 @@
 import os
 
-baaproot = "C:\Users\SAKET\Desktop\QuestionPapers"
-for file in os.listdir(baaproot):
-	root  =  baaproot + "\\" + file
+baaproot = "/home/aman/Desktop/PaperDownloader/QuestionPapers"
+for file in sorted(os.listdir(baaproot)):
+	root  =  baaproot + "/" + file
 	some_var = "aaaaaa"
-	for file in os.listdir(root):
+	print file
+	for file in sorted(os.listdir(root)):
 		if file.endswith(".pdf"):
 			some_var1 = file[:6].upper()
+			# print file
 			if some_var == some_var1:
-				oldpath = root + "\\" + file
-				newpath = root + "\\" + some_var1 + "\\" + file
+				# print "going into not making director"
+				oldpath = root + "/" + file
+				newpath = root + "/" + some_var1 + "/" + file
 				os.rename(oldpath,newpath) 
 			else:
-				os.makedirs(root + "\\" + some_var1)
-				oldpath = root + "\\" + file
-				newpath = root + "\\" + some_var1 + "\\" + file
+				os.makedirs(root + "/" + some_var1)
+				oldpath = root + "/" + file
+				newpath = root + "/" + some_var1 + "/" + file
 				os.rename(oldpath,newpath) 
 				some_var = some_var1
 
